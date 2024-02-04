e90adcc2ccf25a0a9916ef8b3ffcc0855e4e1b29
peterN12g <peternguyensmhs@gmail.com>
2024-02-03 23:47:37 -0600

    Fixed PWA Button Format

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,51 @@
-  <style>
-    body {
-      background-color: beige;
-      margin: 0;
-    }
-    main {
-      background-color: beige; 
-      color: gray;
-    }
-    h1{
-        text-align: center;
-    }
-    p{
-        text-align: center;
-    }
-    div {
-        top: 18%;
-        left: 50%;
-        margin: 0;
-        position: absolute;
-        transform: translate(-50%, -50%);
-    }
-    button{
-        border-radius: 20%;
-        font-size: 20px;
-        background-color: white;
-        transition-duration: 0.4;
-    }
-    button:hover {
-        background-color: white;
-        color: gray;
-    }
-  </style>
+<style>
+  body {
+background-color: beige;
+margin: 0;
+}
+
+main {
+background-color: beige; 
+color: gray;
+text-align: center;
+}
+
+h1 {
+text-align: center;
+}
+
+p {
+text-align: center;
+}
+
+.lifts {
+display: flex;
+justify-content: center;
+align-items: center;
+margin-top: 20px;
+}
+
+button {
+border-radius: 20%;
+font-size: 20px;
+background-color: white;
+transition-duration: 0.4s;
+}
+
+button:hover {
+background-color: white;
+color: gray;
+}
+
+</style>
 <body>
   <main>
     <h1>Powerlifting Warmup Calculator</h1>
     <p>Choose your lift below:</p>
-    <div>
+    <div class="lifts">
       <a href="Squat"><button>Squat</button></a>
       <a href="Bench"><button>Bench</button></a>
       <a href="Deadlift"><button>Deadlift</button></a>
     </div>
   </main>
 </body>
-
-
